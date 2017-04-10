dc20324e0e3c2c134282f550518b1edd0ac55b7c
Anton Kuznetsov <a.kuznetsov@crystalnix.com>
2017-04-10 13:03:18 +0600

    Cleaned up code styling

--- TLTagsContol/TLTagsControl.m
+++ TLTagsContol/TLTagsControl.m
@@ -371,7 +371,7 @@
         [tapDelegate tagsControl:self tappedAtIndex:tapRecognizer.view.tag];
     }
 }
--(void) setKeyboardFocus{
+- (void)setKeyboardFocus {
     [tagInputField_ becomeFirstResponder];
 }
 @end
